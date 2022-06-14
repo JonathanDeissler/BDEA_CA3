1dcb343ccc66813442fc43bbc277290d7623167f
JonathanDeissler <jonathandeissler@gmail.com>
2022-06-14 18:29:05 +0200

    upload enabled

--- flask_proj/app/app.py
+++ flask_proj/app/app.py
@@ -1,20 +1,21 @@
-from flask import Flask, render_template
-
+import os
 from datetime import timedelta
 
 # needed for any cluster connection
 from couchbase.auth import PasswordAuthenticator
 from couchbase.cluster import Cluster
 # needed for options -- cluster, timeout, SQL++ (N1QL) query, etc.
-from couchbase.options import (ClusterOptions, ClusterTimeoutOptions,
-                               QueryOptions)
+from couchbase.options import (ClusterOptions, QueryOptions)
 
+from flask import Flask, render_template, request
+import pandas as pd
+import csv
 
 app = Flask(__name__)
-
-
+cb_coll = None
 @app.route('/')
 def index():  # put application's code here
+
     tweet = {
         "author": "katyperry",
         "content": "@barackobama Thank you for your incredible grace in leadership and for being an exceptional… https://t.co/ZuQLZpt6df",
@@ -27,16 +28,7 @@ def index():  # put application's code here
         "number_of_likes": 6969,
         "number_of_shares": 1380
     }
-    # upsert document function
-    def upsert_document(doc, cb_coll):
-        print("\nUpsert CAS: ")
-        try:
-            # key will equal: "airline_8091"
-            key = doc["author"] + "_" + str(doc["id"])
-            result = cb_coll.upsert(key, doc)
-            return result.cas
-        except Exception as e:
-            return (e)
+
 
     # get document function
 
@@ -85,17 +77,18 @@ def index():  # put application's code here
     cluster.wait_until_ready(timedelta(seconds=5))
 
     # get a reference to our bucket
-    cb = cluster.bucket(bucket_name)
 
+    cb = cluster.bucket(bucket_name)
+    global cb_coll
     cb_coll = cb.scope("_default").collection("_default")
 
     # Get a reference to the default collection, required for older Couchbase server versions
     cb_coll_default = cb.default_collection()
     print("hollo Docker")
 
-    val = upsert_document(tweet, cb_coll)
-    print(val)
-    val = get_airline_by_key(tweet["author"] + "_" + str(tweet["id"]),cb_coll)
+    #val = upsert_document(tweet, cb_coll)
+    #print(val)
+    #val = get_airline_by_key(tweet["author"] + "_" + str(tweet["id"]),cb_coll)
 
     #lookup_by_callsign("CBS",cluster)
 
@@ -104,5 +97,33 @@ def index():  # put application's code here
 @app.route('/action_page')
 def action_page(input=None):
     return "<h1>Biber in Braun nicht in lila</h1>"
+
+
+@app.route('/upload')
+def upload_file():
+    csv_import('../app/ressources/tweets.csv')
+    return '<h1>test</h1>'
+
+def csv_import(filename):
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            cb_coll.upsert(row["author"] + row["date_time"]+ row["number_of_likes"],row)
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def uploader_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        return 'file uploaded successfully'
+
+@app.route('/pandastest')
+def pandas_test():
+    filepath = '../app/ressources/tweets.csv'
+    tweets = pd.read_csv(filepath)
+    val = '<h1>' + tweets.head().to_string() + '</h1>'
+    return val
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
