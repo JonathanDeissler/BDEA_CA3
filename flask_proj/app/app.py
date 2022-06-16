6013735b7234c8e085c2e321f8f5b127f95d9ae7
JonathanDeissler <jonathandeissler@gmail.com>
2022-06-16 10:02:03 +0200

    added Preprocessing and imports

--- flask_proj/app/app.py
+++ flask_proj/app/app.py
@@ -1,4 +1,4 @@
-import os
+import json
 from datetime import timedelta
 
 # needed for any cluster connection
@@ -6,6 +6,7 @@ from couchbase.auth import PasswordAuthenticator
 from couchbase.cluster import Cluster
 # needed for options -- cluster, timeout, SQL++ (N1QL) query, etc.
 from couchbase.options import (ClusterOptions, QueryOptions)
+import couchbase.subdocument as SD
 
 from flask import Flask, render_template, request
 import pandas as pd
@@ -13,6 +14,7 @@ import csv
 
 app = Flask(__name__)
 cb_coll = None
+cluster = None
 @app.route('/')
 def index():  # put application's code here
 
@@ -29,32 +31,8 @@ def index():  # put application's code here
         "number_of_shares": 1380
     }
 
-
-    # get document function
-
-    def get_airline_by_key(key, cb_coll):
-        print("\nGet Result: ")
-        try:
-            result = cb_coll.get(key)
-            return (result.content_as[str])
-        except Exception as e:
-            print(e)
-
-    # query for new document by callsign
-
-    def lookup_by_callsign(cs, cluster):
-        print("\nLookup Result: ")
-        try:
-            sql_query = 'SELECT VALUE name FROM `travel-sample`.inventory.airline WHERE callsign = $1'
-            row_iter = cluster.query(
-                sql_query,
-                QueryOptions(positional_parameters=[cs]))
-            for row in row_iter:
-                print(row)
-        except Exception as e:
-            print(e)
     # Update this to your cluster
-    username = "Administrator"
+    username = "admin"
     password = "123456"
     bucket_name = "Tweets"
     cert_path = "path/to/certificate"
@@ -71,6 +49,7 @@ def index():  # put application's code here
 
     # Get a reference to our cluster
     # NOTE: For TLS/SSL connection use 'couchbases://<your-ip-address>' instead
+    global cluster
     cluster = Cluster('couchbase://172.17.0.4', ClusterOptions(auth))
 
     # Wait until the cluster is ready for use.
@@ -84,46 +63,82 @@ def index():  # put application's code here
 
     # Get a reference to the default collection, required for older Couchbase server versions
     cb_coll_default = cb.default_collection()
-    print("hollo Docker")
-
-    #val = upsert_document(tweet, cb_coll)
-    #print(val)
-    #val = get_airline_by_key(tweet["author"] + "_" + str(tweet["id"]),cb_coll)
-
-    #lookup_by_callsign("CBS",cluster)
+    print("hallo Docker")
 
     return render_template('index.html')
-
-@app.route('/action_page')
-def action_page(input=None):
-    return "<h1>Biber in Braun nicht in lila</h1>"
-
 
 @app.route('/upload')
 def upload_file():
     csv_import('../app/ressources/tweets.csv')
     return '<h1>test</h1>'
 
+@app.route('/upload_account_new')
+def upload_json_file():
+    json_import('../app/ressources/data.json')
+    return '<h1>test</h1>'
+
+
 def csv_import(filename):
     with open(filename) as csvfile:
         reader = csv.DictReader(csvfile)
+        for idx, row in enumerate(reader):
+            cb_coll.upsert(str(idx), row)
+
+
+def json_import(filename):
+    bucket_name = "Tweets"
+    cb = cluster.bucket(bucket_name)
+    cb_coll_lokal = cb.scope("_default").collection("new_accounts")
+    with open(filename, 'r') as f:
+        data = json.load(f)
+        for ele in data:
+            cb_coll_lokal.upsert(ele["user"],ele)
+
+@app.route('/upload_con')
+def upload_connections():
+    txt_import('ressources/twitter_combined.csv')
+    return '<h1>test</h1>'
+
+
+def txt_import(filename):
+    bucket_name = "Tweets"
+    cb = cluster.bucket(bucket_name)
+    cb_coll_lokal = cb.scope("_default").collection("accounts")
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=' ')
         for row in reader:
-            cb_coll.upsert(row["author"] + row["date_time"]+ row["number_of_likes"],row)
+
+            try:
+                old_value = cb_coll_lokal.get(row["user"])
+            except:
+                old_value = None
+            if not old_value:
+                cb_coll_lokal.upsert(row["user"], [row["follow"]])
+            else:
+                cb_coll_lokal.mutate_in(row["user"], (SD.array_append("", row["follow"]),))
 
 
-@app.route('/uploader', methods=['GET', 'POST'])
-def uploader_file():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save(f.filename)
-        return 'file uploaded successfully'
-
-@app.route('/pandastest')
+@app.route('/query', methods=['POST'])
 def pandas_test():
-    filepath = '../app/ressources/tweets.csv'
-    tweets = pd.read_csv(filepath)
-    val = '<h1>' + tweets.head().to_string() + '</h1>'
+    query = request.form['myquery']
+    val = lookup_query(cluster, query)
     return val
+
+
+def lookup_query(cluster, query):
+    print("\nLookup Result: ")
+    try:
+        sql_query = query
+        row_iter = cluster.query(
+            sql_query)
+        test_string = ""
+        for row in row_iter:
+            test_string = test_string + str(row)
+        test_string = '<h1>' + test_string + '</h1>'
+        return test_string
+    except Exception as e:
+        return '<h1>' + str(e) + '</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
---
+++ flask_proj/preprocessing.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+filename = "app/ressources/twitter_combined_short.csv"
+
+following = {
+}
+
+with open(filename) as file:
+    for row in file:
+        row = row.replace("\n","").split(" ")
+        old_val = following.get(row[0])
+        if old_val:
+            old_val.append(row[1])
+            following[row[0]] = old_val
+        else:
+            following[row[0]] = [row[1]]
+
+followers = {
+}
+
+with open(filename) as file:
+    for row in file:
+        row = row.replace("\n","").split(" ")
+        old_val = followers.get(row[1])
+        if old_val:
+            old_val.append(row[0])
+            followers[row[1]] = old_val
+            #print(followers[row[1]])
+        else:
+            followers[row[1]] = [row[0]]
+
+df = pd.DataFrame(columns=["user","followers","following"])
+for user in followers:
+    following_user = following.get(user)
+    df2 = pd.DataFrame([[user,followers[user],following_user]],columns=["user","followers","following"])
+    df = pd.concat([df,df2])
+
+json_file = df.to_json(orient='records')
+
+import json
+with open('app/ressources/data_short.json', 'a') as f:
+    pass
+    f.write(json_file + '\n')
+
+with open('app/ressources/data_short.json', 'r') as f:
+    data = json.load(f)
+    for ele in data:
+        print(ele)
